5bc4761236fad64cd0d5fb144039336a49df6f19
Arbaaz Kagdi <arbaazkagdi@gmail.com>
2026-07-01 19:30:11 +0530

    started to add animation and tailwind integration

--- src/components/TVShowListItem/TVShowListItem.jsx
+++ src/components/TVShowListItem/TVShowListItem.jsx
@@ -9,16 +9,14 @@ export function TVShowListItem({ tvshow, onClick }) {
   };
 
   return (
-    <div className={s.container} onClick={onItemClick}>
+    <div className={`${s.container} hover:scale-105 hover:z-10 transition-transform duration-300 ease-in-out`} onClick={onItemClick}>
       <img
         src={SMALL_IMG_COVER_BASE_URL + tvshow.backdrop_path}
         alt={tvshow.name || tvshow.title}
         className={s.img}
       />
       <div className={s.title}>
-        {(tvshow.name || tvshow.title).length > max_title_char
-          ? (tvshow.name || tvshow.title).slice(0, max_title_char) + "..."
-          : (tvshow.name || tvshow.title)}
+        {tvshow.name || tvshow.title}
       </div>
     </div>
   );
